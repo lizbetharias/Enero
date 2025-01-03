99bd5f08533d235bc787642b67adcc6270c703f3
lizbetharias <164087987+lizbetharias@users.noreply.github.com>
2025-01-02 22:04:50 -0600

    Add files via upload

---
+++ Paciente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinica.Models;
+
+public partial class Paciente
+{
+    [Key]
+    [Column("pacienteID")]
+    public int PacienteId { get; set; }
+
+    [Column("nombre")]
+    [StringLength(50)]
+    public string Nombre { get; set; } = null!;
+
+    [Column("apellido")]
+    [StringLength(50)]
+    public string Apellido { get; set; } = null!;
+
+    [Column("fechaNacimiento")]
+    public DateOnly? FechaNacimiento { get; set; }
+
+    [Column("direccion")]
+    [StringLength(100)]
+    public string? Direccion { get; set; }
+
+    [Column("telefono")]
+    [StringLength(15)]
+    public string? Telefono { get; set; }
+
+    [Column("email")]
+    [StringLength(50)]
+    public string? Email { get; set; }
+
+    [Column("especialidad")]
+    [StringLength(20)]
+    public string? Especialidad { get; set; }
+
+    [InverseProperty("Paciente")]
+    public virtual ICollection<Examen> Examen { get; set; } = new List<Examen>();
+
+    [InverseProperty("Paciente")]
+    public virtual ICollection<Receta> Receta { get; set; } = new List<Receta>();
+}
